14f659cd9aaaa02af08bb8c4b7c87ccf76a8d4d0
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-03-11 09:24:12 +0100

    Finalize the Shiny app.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Statistics-on-Internet-usage
+## Statistics on Internet usage
+
+This repository contains a simple statistical application written in Shiny.
+The application uses a dataset downloaded from the website of the World Bank, and shows some simple statistics about the Internet usage in Germany.
--- app.R
+++ app.R
@@ -1,47 +1,72 @@
 # R application that displays some basic statistics on Internet usage in Germany between 2000 and 2023
 
+library(tidyverse)
 library(shiny)
 library(ggplot2)
 library(bslib)
-library(shinythemes)
+#library(shinythemes)
+library(rio)
+library(dplyr)
+library(readr)
+
+ds <- rio::import("data/internet_usage.csv")
+
+View(ds)
+
+df <- t(ds) %>% as.data.frame(df)
+
+colnames(df) <- df[1, ]
+df <- df[-1, ]
+str(df)
+
+df <- df %>%
+  mutate_at(c('female','male'),funs(str_replace(., "..", "0")))
+str(df)
+
+df$year = parse_number(df$year, )
+
+df$female = parse_number(df$female, )
+
+df$male = parse_number(df$male, )
+
+# df %>%
+#   mutate(year = as.numeric(year))
+# df %>%
+#   mutate(female = as.numeric(female))
+# df %>%
+#   mutate(male = as.numeric(male))
+
+View(df)
+str(df)
 
 # **************************** User Interface ****************************
 
-ui <- 
+ui <-
   page_fluid(
-    theme = bs_theme(bootswatch = "darkly"),
+    theme = bs_theme(bootswatch = "minty"),
     layout_sidebar(
       sidebar = sidebar(
-        sliderInput("bins", "Number of bins:", min = 5, max = 50, value = 25),
-        selectInput("color", "Choose a color:", choices = c("Blue" = "#007bc2", "Red" = "#c20000", "Green" = "#00c244")),
-        selectInput("themaAbend", "Choose a theme:", choices = c("Classic", "Minimal", "Dark"))
+        sliderInput("year_interval", "Years:", min = 2000, max = 2023, sep = "", value = c(2000, 2023))
+      ),
+      layout_columns(
+        col_widths = c(12),
+        card(
+          helpText(
+            h2("Average internet usage in Germany between 2000 and 2023")
+          )  
+        )
       ),
       layout_columns(
         col_widths = c(6, 6),
-        value_box("Average internet usage by women", textOutput("mean_usage_women")),
-        value_box("Average internet usage by men", textOutput("mean_usage_men")),
+        value_box("By women", textOutput("mean_usage_women")),
+        value_box("By men", textOutput("mean_usage_men")),
       ),
       layout_columns(
-        col_widths = c(6, 6),
+        col_widths = c(12),
         card(
-          card_header("Histogram"),
+          card_header("Internet usage in Germany by women and men"),
           full_screen = T,
-          card_body(plotOutput("distPlot"))
-        ),
-        card(
-          card_header("Density Plot"),
-          full_screen = T,
-          card_body(plotOutput("densityPlot"))
-        ),
-        card(
-          card_header("Box Plot"),
-          full_screen = T,
-          card_body(plotOutput("boxPlot"))
-        ),
-        card(
-          card_header("Scatter Plot"),
-          full_screen = T,
-          card_body(plotOutput("scatterPlot"))
+          card_body(plotOutput("lineplot"))
         )
       )
     )
@@ -51,58 +76,21 @@ ui <-
 # *************************** Server logic *****************************
 
 server <- function(input, output) {
-  theme_choice <- reactive({
-    switch(input$themaAbend,
-           "Classic" = theme_classic(),
-           "Minimal" = theme_minimal(),
-           "Dark" = theme_dark())
+  theme_choice <- reactive(theme_light())
+  
+  output$mean_usage_women <- renderText({
+    paste(round(mean(df$female), 2), "percent")
   })
-  
-  output$mean_waiting <- renderText({
-    paste(round(mean(faithful$waiting), 2), "mins")
+
+  output$mean_usage_men <- renderText({
+    paste(round(median(df$male), 2), "percent")
   })
-  
-  output$median_waiting <- renderText({
-    paste(round(median(faithful$waiting), 2), "mins")
-  })
-  
-  output$eruption_count <- renderText({
-    paste(nrow(faithful))
-  })
-  
-  
-  output$distPlot <- renderPlot({
-    x <- faithful$waiting
-    binjeink <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    ggplot(data.frame(x), aes(x)) +
-      geom_histogram(breaks = binjeink, fill = input$color, color = "white") +
-      labs(x = "Waiting time to next eruption (in mins)", y = "Frequency") +
-      theme_choice()
-  })
-  
-  output$densityPlot <- renderPlot({
-    x <- faithful$waiting
-    
-    ggplot(data.frame(x), aes(x)) +
-      geom_density(fill = input$color, alpha = 0.5) +
-      labs(title = "Density Plot of Waiting Times", x = "Waiting Time (mins)", y = "Density") +
-      theme_choice()
-  })
-  
-  output$boxPlot <- renderPlot({
-    x <- faithful$waiting
-    
-    ggplot(data.frame(x), aes(y = x)) +
-      geom_boxplot(fill = input$color, color = "black") +
-      labs(title = "Boxplot of Waiting Times", y = "Waiting Time (mins)") +
-      theme_choice()
-  })
-  
-  output$scatterPlot <- renderPlot({
-    ggplot(faithful, aes(x = eruptions, y = waiting)) +
-      geom_point(color = input$color) +
-      labs(title = "Scatter Plot of Eruptions vs Waiting Time", x = "Eruption Duration (mins)", y = "Waiting Time (mins)") +
+
+  output$lineplot <- renderPlot({
+    df %>% filter(between(year, input$year_interval[1], input$year_interval[2])) %>%
+      pivot_longer(cols = -year) %>%
+    ggplot(aes(x = year, y = value, group = name, color = name)) + 
+      geom_line(size=2, alpha=0.9, linetype=1) + labs(title = "Internet usage in Germany based on gender", x = "Year", y = "Percentage") +
       theme_choice()
   })
 }
@@ -111,4 +99,13 @@ server <- function(input, output) {
 
 # ************************* Run the application *************************
 
-shinyApp(ui, server, options = list(display.mode = "showcase"))
+ shinyApp(ui, server, options = list(display.mode = "showcase"))
+ 
+ 
+ 
+ 
+ 
+ # df %>%
+ #  pivot_longer(cols = -year) %>%
+ #  ggplot(aes(x = year, y = value, group = name, color = name)) + geom_line() + labs(title = "Density Plot of Internet usage", x = "Year", y = "Percentage") +
+ #   theme_choice()
